6fd29fa0403683fcf085fd046e3a5f807cac05f0
Benoit LE BOURDONNEC <benoitlebourdonnec@MacBook-Pro-2.local>
2021-01-13 16:56:49 +0100

    add login

---
+++ controllers/login.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const connection = require('../config/connection');
+
+const { CLIENT_URL, JWT_SECRET } = process.env;
+
+const app = express();
+
+app.use(
+  cors({
+    origin: CLIENT_URL,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  if (email === '' || password === '') {
+    res.status(400).send('Please specify your email or password');
+  } else {
+    connection.query(
+      'SELECT * FROM user WHERE email=?',
+      [email],
+      (err, result) => {
+        if (err) {
+          res.status(500).json({
+            error: err.message,
+            sql: err.sql,
+          });
+        } else if (result.length === 0) {
+          res.status(403).send('Invalid email');
+        } else if (bcrypt.compareSync(password, result[0].password)) {
+          const token = jwt.sign(
+            {
+              id: result.id,
+            },
+            JWT_SECRET,
+            { expiresIn: '1h' }
+          );
+          const user = {
+            id: result[0].id,
+            email,
+            password: 'hidden',
+          };
+          res.status(200).json({ user, token });
+        } else {
+          res.status(403).send('invalid password');
+        }
+      }
+    );
+  }
+});
