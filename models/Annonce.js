2d09997b424e60ed489dc4bcb7345e0ad6653983
Florian Milcendeau <fmilcendeau85@gmail.com>
2021-01-14 22:51:25 +0100

    fix: sql query

--- models/Annonce.js
+++ models/Annonce.js
@@ -7,7 +7,7 @@ class Annonce {
 
   static findOneById(id) {
     return new Promise((resolve, reject) => {
-      const sql = `SELECT a.id, a.duree, a.debut, a.prix, a.localisation, a.niveau_etudes, a.langage, a.description, a.type, a.expertise, a.slug, a.disponible 
+      const sql = `SELECT a.id, a.duree, a.debut, a.prix, a.localisation, a.niveau_etudes, a.langage, a.description, a.type, a.expertise, a.slug, a.disponible, c.nom, c.adresse, c.mail, c.telephone, c.domaine, c.logo, c.logo_small 
                     FROM ${this.table} AS a 
                     JOIN ${this.companyFK} AS c 
                     ON c.id = a.entreprises_id  
