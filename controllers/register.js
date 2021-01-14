91a61e765a50bc5dc73f721542b8439889ef67cc
nicolas <nicolas.mercier570@gmail.com>
2021-01-14 11:23:25 +0100

    register route for user

--- controllers/register.js
+++ controllers/register.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
+const { JWT_SECRET } = process.env;
 const router = express.Router();
 const connection = require('../config/connection');
 
-router.post('/', (req, res) => {
+router.post('/user', (req, res) => {
   const { email, password, nom, prenom } = req.body;
 
   if (email === '' || password === '' || nom === '' || prenom === '') {
@@ -22,11 +24,32 @@ router.post('/', (req, res) => {
             sql: err.sql,
           });
         } else {
-          res.status(201).json({
-            id: result.insertId,
-            email,
-            password: hashpassword,
-          });
+          connection.query(
+            'SELECT * FROM users WHERE id=?',
+            [result.insertId],
+            (error, results) => {
+              if (err) {
+                res.status(500).json({
+                  error: err.message,
+                  sql: err.sql,
+                });
+              } else {
+                const token = jwt.sign(
+                  {
+                    id: result.id,
+                  },
+                  JWT_SECRET,
+                  { expiresIn: '1h' }
+                );
+                const user = {
+                  id: results[0].id,
+                  email,
+                  password: 'hidden',
+                };
+                res.status(201).json({ user, token });
+              }
+            }
+          );
         }
       }
     );
@@ -34,3 +57,8 @@ router.post('/', (req, res) => {
 });
 
 module.exports = router;
+// res.status(201).json({
+//   id: result.insertId,
+//   email,
+//   password: hashpassword,
+// });
